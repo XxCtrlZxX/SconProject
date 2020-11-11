1611de54c6911bd6157d52fa3216b88aa84f2913
Jung haesung <48352078+XxCtrlZxX@users.noreply.github.com>
2020-11-11 15:17:22 +0900

    laptop 1111

--- app/src/main/java/com/example/scon_rlaruddhks/MainActivity.java
+++ app/src/main/java/com/example/scon_rlaruddhks/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.scon_rlaruddhks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -16,29 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction tran = getSupportFragmentManager().beginTransaction();
         bottomNavigationView = findViewById(R.id.bottom_nav);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.nav_home:
-                    tran.replace(R.id.container, HomeFragment.newInstance());
-                    Toast.makeText(MainActivity.this, String.valueOf(item.getItemId()), Toast.LENGTH_SHORT).show();
+                    ChangeFragment(HomeFragment.newInstance());
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.nav_ranking:
-                    tran.replace(R.id.container, RankingFragment.newInstance());
-                    Toast.makeText(MainActivity.this, String.valueOf(item.getItemId()), Toast.LENGTH_SHORT).show();
+                    ChangeFragment(RankingFragment.newInstance());
+                    Toast.makeText(MainActivity.this, "Ranking", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.nav_highlight:
-                    tran.replace(R.id.container, HighLightFragment.newInstance());
-                    Toast.makeText(MainActivity.this, String.valueOf(item.getItemId()), Toast.LENGTH_SHORT).show();
+                    ChangeFragment(HighLightFragment.newInstance());
+                    Toast.makeText(MainActivity.this, "Highlight", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.nav_toto:
-                    tran.replace(R.id.container, TotoFragment.newInstance());
-                    Toast.makeText(MainActivity.this, String.valueOf(item.getItemId()), Toast.LENGTH_SHORT).show();
+                    ChangeFragment(TotoFragment.newInstance());
+                    Toast.makeText(MainActivity.this, "Toto", Toast.LENGTH_SHORT).show();
                     break;
             }
             return true;
         });
+    }
+
+    private void ChangeFragment(Fragment fragment) {
+        FragmentTransaction tran = getSupportFragmentManager().beginTransaction();
+        tran.replace(R.id.container, fragment);
+        tran.commit();
     }
 }
