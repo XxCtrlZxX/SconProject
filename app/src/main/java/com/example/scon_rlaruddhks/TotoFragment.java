42f46b2b9c00142cd519246b6f5b1c134c58f788
XxCtrlZxX <harryjung1001@gmail.com>
2020-11-26 23:15:43 +0900

    재구성

--- app/src/main/java/com/example/scon_rlaruddhks/TotoFragment.java
+++ app/src/main/java/com/example/scon_rlaruddhks/TotoFragment.java
@@ -8,28 +8,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class TotoFragment extends Fragment {
+
     public static TotoFragment newInstance() {
         return new TotoFragment();
     }
 
-    ImageButton lup,ldown,rup,rdown;
-    TextView ltext, rtext;
-    int left_num = 0, right_num = 0;
     private Context mContext;
+
+    NumberPicker score1, score2;
     Button submit;
+
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         mContext = context;
-
     }
 
     @Override
@@ -37,52 +41,23 @@ public class TotoFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_toto, container, false);
 
-        lup = view.findViewById(R.id.left_up);
-        rup = view.findViewById(R.id.right_up);
-        ldown = view.findViewById(R.id.left_down);
-        rdown = view.findViewById(R.id.right_down);
+        score1 = view.findViewById(R.id.score_pick1);
+        score2 = view.findViewById(R.id.score_pick2);
 
-        ltext = view.findViewById(R.id.score_num1);
-        rtext = view.findViewById(R.id.score_num2);
+        score1.setMaxValue(20);
+        score1.setMinValue(0);
+        score2.setMaxValue(20);
+        score2.setMinValue(0);
 
         submit = view.findViewById(R.id.submit);
-
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // ltext, rtext를 파이어배이스로 전달
-            }
+        submit.setOnClickListener(view1 -> {
+            Toast.makeText(mContext, score1.getValue() + " : " + score2.getValue(), Toast.LENGTH_SHORT).show();
         });
-
-        lup.setOnClickListener(v -> {
-            left_num++;
-            ltext.setText(left_num + "");
-        });
-        ldown.setOnClickListener(v -> {
-
-            if(left_num > 0){
-                left_num--;
-            }
-            else {
-                Toast.makeText(mContext, "점수는 음수가 될 수 없습니다", Toast.LENGTH_SHORT).show();
-            }
-            ltext.setText(left_num + "");
-        });
-        rup.setOnClickListener(v -> {
-            right_num++;
-            rtext.setText(right_num+ "");
-        });
-        rdown.setOnClickListener(v -> {
-            if (right_num > 0) {
-                right_num--;
-            }
-            else {
-                Toast.makeText(mContext, "점수는 음수가 될 수 없습니다", Toast.LENGTH_SHORT).show();
-            }
-            rtext.setText(right_num+ "");
-        });
-        
 
         return view;
+    }
+
+    private void LoadScore() {
+
     }
 }
